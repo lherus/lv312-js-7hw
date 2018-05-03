7f3fd6277e7589d1f3efbb900eb97594477ff96d
Lyudmyla Herus <lyusi.lv@gmail.com>
2018-05-03 23:04:21 +0000

    hw

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
-let a = 1, b = 1, n = 15;
-document.write(a +  '<br>');
-document.write(b +  '<br>');
-    let j = 2, i = 2;
-    while (i < n) {
-        j = a + b;
-        a = b;
-        b = j;
-        document.write(" " + j + '<br>');
-        i++;
+// let a = 1, b = 1, n = 15;
+// document.write(a +  '<br>');
+// document.write(b +  '<br>');
+//     let j = 2, i = 2;
+//     while (i < n) {
+//         j = a + b;
+//         a = b;
+//         b = j;
+// document.write(" " + j + '<br>');
+//         i++;
+// }
+
+
+let n = parseFloat(prompt('Enter a number'));
+function fib(n) {
+    if (n <= 1) {
+        return n;
+    } return fib(n-1) + fib(n-2);
 }
+document.write(fib(n));
